869d5badba480f064786647aa6a0e84fa299fa16
Anastasia Kraevaya <42145078+akraevaya@users.noreply.github.com>
2018-09-12 15:41:07 +0300

    подготовка ко второй проверке

--- js/script.js
+++ js/script.js
@@ -46,6 +46,8 @@ close.addEventListener("click", function(evt) {
 form.addEventListener("submit", function(evt) {
   if (!userName.value || !email.value || !message.value) {
     evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("modal-error");
   } else {
     if (isStorageSupport) {
